496f4759df4128c2a041cc2e3efa181a7647aa7e
sun-amy <111322085+sun-amy@users.noreply.github.com>
2022-08-18 15:27:47 +0100

    Create initials.py

---
+++ initials.py
@@ -0,0 +1,22 @@
+"""
+In this project, Codecademy asks that I print my initials in block letters.
+"""
+
+block_a = """  A  
+ A A 
+AAAAA
+A   A
+A   A
+A   A
+"""
+
+block_s = """ SSS 
+S   S
+S
+ SSS
+    S
+S   S
+ SSS
+ """
+
+print(block_a + block_s)
